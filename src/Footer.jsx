527bab22073d2a1737b3aa4af6986a052a258bda
Kidd <kiddochang02@gmail.com>
2023-01-05 21:46:08 -0500

    Give credit to images

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,12 +6,11 @@ function Footer() {
         <div id="footer">
             <ul className="copyright">
                 <li>&copy; Kidd Chang 2023</li>
-                <li>Design: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li>
                 <li>
-                    Photo by <a href="https://unsplash.com/@snapsbyclark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Clark Van Der Beken</a> on <a href="https://unsplash.com/photos/Tk0B3Dfkf_4" target="_blank">Unsplash</a>
+                    Photo by <a href="https://unsplash.com/@mitchel3uo" target="_blank">Mitchell Luo</a> on <a href="https://unsplash.com/photos/xREzOtWzDrQ" target="_blank">Unsplash</a>
                 </li>
                 <li>
-                    Photo by <a href="https://unsplash.com/@timmossholder?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Tim Mossholder</a> on <a href="https://unsplash.com/photos/GOMhuCj-O9w" target="_blank">Unsplash</a>
+                    Photo by <a href="https://www.andygreenwell.com" target="_blank">Andy GreenWell</a> on <a href="https://www.andygreenwell.com/wp-content/uploads/2017/01/business-headshot-photography-dearborn_31483-960x1289.jpg" target="_blank">Andy Greenwell Photography Inc</a>
                 </li>
             </ul>
         </div>
